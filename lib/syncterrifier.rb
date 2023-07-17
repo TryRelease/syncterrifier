bc87bedf48b54ffcfa969cecf9555e5687a7084e
Bradley Herman <bradley.t.herman@gmail.com>
2023-07-17 13:37:18 -0700

    Init

---
+++ lib/syncterrifier.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "syncterrifier/version"
+
+module Syncterrifier
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/syncterrifier/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Syncterrifier
+  VERSION = "0.1.0"
+end
