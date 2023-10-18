ce094a38a6eb7b3d0a5c59e32d431a9827a7bad3
Bradley Herman <bradley.t.herman@gmail.com>
2023-10-18 14:38:31 -0700

    Add model for verifications

---
+++ lib/syncterrifier/models/verifications/verification.rb
@@ -0,0 +1,34 @@
+require_relative '../../model'
+
+<<-DOC
+  Possible params:
+    customer_ip_address
+    document_id
+
+    customer_consent - boolean (required)
+    person_id - string (required if no business_id)
+    business_id - string (required if no person_id)
+DOC
+
+class Syncterrifier::Verification < Syncterrifier::Model
+  endpoint :verifications
+
+  required_params(
+    customer_consent: :boolean,
+    status: [
+      'ACTIVE',
+      'DECEASED',
+      'DENIED',
+      'DORMANT',
+      'ESCHEAT',
+      'FROZEN',
+      'INACTIVE',
+      'PROSPECT',
+      'SANCTION'
+    ]
+  )
+
+  def self.verify(idempotency_key: nil, **data)
+    client.post("#{ url }/verify", data, idempotency_key: idempotency_key)
+  end
+end
