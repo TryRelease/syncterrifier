bc29f57a72adb86719293f7bb0944780ebeb0474
Bradley Herman <bradley.t.herman@gmail.com>
2024-07-16 22:22:42 -0700

    Fix validator

--- lib/uuid_validator.rb
+++ lib/uuid_validator.rb
@@ -1,5 +1,5 @@
 class UuidValidator
-  def validate(uuid)
+  def self.validate(uuid)
     uuid_regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
     return true if uuid_regex.match?(uuid.to_s.downcase)
   end
