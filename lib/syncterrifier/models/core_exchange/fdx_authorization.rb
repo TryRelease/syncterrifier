c68424aecfc931d34131ac31913aaa3364f94502
Bradley Herman <bradley.t.herman@gmail.com>
2024-07-24 12:53:43 -0700

    Fix fdx auth api

--- lib/syncterrifier/models/core_exchange/fdx_authorization.rb
+++ lib/syncterrifier/models/core_exchange/fdx_authorization.rb
@@ -5,17 +5,28 @@ class Syncterrifier::FdxAuthRequest < Syncterrifier::Model
 
   uses_v1!
 
-  def self.authorize(customer_id:, auth_request_id:, status:)
-    validate_data!(customer_id:, auth_request_id:, status:)
-    self.new(client.post("fdx_auth_requests/authorize", {
-      customer_id: customer_id,
+  def self.authorize(customer_id: nil, auth_request_id:, status:, business_id: nil)
+    validate_data!(customer_id:, auth_request_id:, status:, business_id:)
+    params = {
       auth_request_id: auth_request_id,
       status: status
-    }, use_v1: true))
+    }
+    params[:customer_id] = customer_id if customer_id
+    params[:business_id] = business_id if business_id
+
+    self.new(client.post("fdx_auth_requests/authorize", params, use_v1: true))
   end
 
-  def self.validate_data!(customer_id:, auth_request_id:, status:)
-    raise "customer_id (UUID) is required" unless UuidValidator.validate(customer_id)
+  def self.validate_data!(customer_id: nil, auth_request_id:, status:, business_id: nil)
+    if customer_id && !UuidValidator.validate(customer_id)
+      raise "customer_id (UUID) is required"
+    end
+    if business_id && !UuidValidator.validate(business_id)
+      raise "business_id (UUID) is required"
+    end
+    if !business_id && !customer_id
+      raise "business_id or customer_id is required"
+    end
     raise "auth_request_id (string) is required" unless auth_request_id.is_a?(String) && auth_request_id.length > 0
     raise "status is required" unless status.is_a?(String) && status.length > 0
     raise "status must be DENIED/GRANTED/ERROR" unless %w[DENIED GRANTED ERROR].include?(status)
